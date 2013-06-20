da1295bc4daf29624f0c2618e734ee654dae28af
Newptone <yuxcer@gmail.com>
2013-06-20 06:07:30 -0400

    Update template variable to use @,and add storebackend in puppet.conf Add add foreman module.

---
+++ puppet/modules/foreman/manifests/config.pp
@@ -0,0 +1,51 @@
+class foreman::config {
+  Cron {
+    require     => User[$foreman::user],
+    user        => $foreman::user,
+    environment => "RAILS_ENV=${foreman::environment}",
+  }
+
+  file {'/etc/foreman/settings.yaml':
+    content => template('foreman/settings.yaml.erb'),
+    notify  => Class['foreman::service'],
+    owner   => $foreman::user,
+    require => User[$foreman::user],
+  }
+
+  case $::operatingsystem {
+    Debian,Ubuntu: {
+      $init_config = '/etc/default/foreman'
+      $init_config_tmpl = 'foreman.default'
+    }
+    default: {
+      $init_config = '/etc/sysconfig/foreman'
+      $init_config_tmpl = 'foreman.sysconfig'
+    }
+  }
+  file { $init_config:
+    ensure  => present,
+    content => template("foreman/${init_config_tmpl}.erb"),
+    require => Class['foreman::install'],
+    before  => Class['foreman::service'],
+  }
+
+  file { $foreman::app_root:
+    ensure  => directory,
+  }
+
+  user { $foreman::user:
+    ensure  => 'present',
+    shell   => '/sbin/nologin',
+    comment => 'Foreman',
+    home    => $foreman::app_root,
+    require => Class['foreman::install'],
+  }
+
+  # remove crons previously installed here, they've moved to the package's
+  # cron.d file
+  cron { ['clear_session_table', 'expire_old_reports', 'daily summary']:
+    ensure  => absent,
+  }
+
+  if $foreman::passenger  { include foreman::config::passenger }
+}
---
+++ puppet/modules/foreman/spec/fixtures/modules/foreman/manifests/params.pp
@@ -0,0 +1,74 @@
+class foreman::params {
+
+# Basic configurations
+  $foreman_url  = "https://${::fqdn}"
+  # Should foreman act as an external node classifier (manage puppet class
+  # assignments)
+  $enc          = true
+  # Should foreman receive reports from puppet
+  $reports      = true
+  # Should foreman recive facts from puppet
+  $facts        = true
+  # Do you use storeconfig (and run foreman on the same database) ? (note: not
+  # required)
+  $storeconfigs = false
+  # should foreman manage host provisioning as well
+  $unattended   = true
+  # Enable users authentication (default user:admin pw:changeme)
+  $authentication = true
+  # configure foreman via apache and passenger
+  $passenger    = true
+  # force SSL (note: requires passenger)
+  $ssl          = true
+
+  # Choose whether you want to enable locations and organizations.
+  $locations_enabled      = false
+  $organizations_enabled  = false
+
+# Advance configurations - no need to change anything here by default
+  # if set to true, no repo will be added by this module, letting you to
+  # set it to some custom location.
+  $custom_repo = false
+  # this can be stable, rc, or nightly
+  $repo        = 'stable'
+  $railspath   = '/usr/share'
+  $app_root    = "${railspath}/foreman"
+  $user        = 'foreman'
+  $environment = 'production'
+  $use_sqlite  = true
+
+  # OS specific paths
+  $ruby_major = regsubst($::rubyversion, '^(\d+\.\d+).*', '\1')
+  case $::operatingsystem {
+    fedora: {
+      if $::operatingsystemrelease >= 17 {
+        $puppet_basedir  = "/usr/share/ruby/vendor_ruby/puppet"
+      } else {
+        $puppet_basedir  = "/usr/lib/ruby/site_ruby/${ruby_major}/puppet"
+      }
+      $apache_conf_dir = '/etc/httpd/conf.d'
+      $yumcode = "f${::operatingsystemrelease}"
+    }
+    redhat,centos,Scientific: {
+      $puppet_basedir  = "/usr/lib/ruby/site_ruby/${ruby_major}/puppet"
+      $apache_conf_dir = '/etc/httpd/conf.d'
+      $osmajor = regsubst($::operatingsystemrelease, '\..*', '')
+      $yumcode = "el${osmajor}"
+    }
+    Debian,Ubuntu: {
+      $puppet_basedir  = '/usr/lib/ruby/vendor_ruby/puppet'
+      $apache_conf_dir = '/etc/apache2/conf.d'
+    }
+    default:              {
+      $puppet_basedir  = "/usr/lib/ruby/${ruby_major}/puppet"
+      $apache_conf_dir = '/etc/apache2/conf.d/foreman.conf'
+    }
+  }
+  $puppet_home = '/var/lib/puppet'
+
+  # If CA is specified, remote Foreman host will be verified in reports/ENC scripts
+  $client_ssl_ca   = "${puppet_home}/ssl/certs/ca.pem"
+  # Used to authenticate to Foreman, required if require_ssl_puppetmasters is enabled
+  $client_ssl_cert = "${puppet_home}/ssl/certs/${fqdn}.pem"
+  $client_ssl_key  = "${puppet_home}/ssl/private_keys/${fqdn}.pem"
+}
